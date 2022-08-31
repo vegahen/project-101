4e83ade3af05f149c7244d1e39388d0bcbd5591b
vegard.hennestad <vegard.hennestad@accenture.com>
2022-08-31 11:42:14 +0200

    wednesday sod orjan

--- backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
+++ backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
@@ -1,72 +1,45 @@
 package no.acntech.project101.web.employee.resources;
-
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-
-import no.acntech.project101.employee.Employee;
-import no.acntech.project101.employee.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 @CrossOrigin(origins = "http://localhost:3000")
-//TODO This is a REST controler and should receive request on path employees
-@RestController
-@RequestMapping("employees")
+@RestController@RequestMapping("employees")
 public class EmployeeResource {
-
-    //TODO The constructor needs to accept the required dependencies and assign them to class variables
-    private final EmployeeService employeeService;
-
-    public EmployeeResource(
-            EmployeeService employeeService
-    ) {
-        this.employeeService = employeeService;
+    public EmployeeResource() {
     }
-
-    @GetMapping
-    public ResponseEntity<List<EmployeeDto>> findAll() {
-
-        //TODO create a GET endpoint find all employees in the database and return them
-        return ResponseEntity.ok(employees)
-        return null;
+    @GetMapping    public ResponseEntity<List<EmployeeDto>> findAll() {
+        return ResponseEntity.ok(Collections.emptyList());
     }
-
     @GetMapping("{id}")
-    public ResponseEntity<EmployeeDto> findById(@PathVariable long id) {
-        Optional<Employee> employee = employeeService.findById(id);
-        // TODO create a GET endpoint that fetches a spesific employee based on its ID
-        final var person = new EmployeeDto(1L, "Ørjan", "Johansen", LocalDate.of(1985, 1, 5), 1337);
-        return responseEntity.ok(person);
+    public ResponseEntity<EmployeeDto> findById(@PathVariable final Long id) {
+        final var employee1 = new EmployeeDto(1L, "Ørjan", "Johansen", LocalDate.of(1985, 1, 24), 1L);
+        return ResponseEntity.ok(employee1);
     }
-
-    @postmapping
-    public ResponseEntity createEmployee(@RequestBody EmployeeDto employeeDto) {
-
-        long id = 1337;
-
-        ServletUriComponentsBuilder.fromCurrentRequest()
+    @PostMapping    public ResponseEntity createEmployee(@RequestBody @Validated final EmployeeDto employeeDto) {
+        final var uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
-                .buildAndExpand(id)
+                .buildAndExpand(1L)
                 .toUri();
-
-        //TODO Create a POST endpoint that accepts an employeeDTO and saves it in the database
-
         return ResponseEntity.created(uri).build();
     }
-
     @DeleteMapping("{id}")
-    public ResponseEntity deleteEmployee(@PathVariable Long id) {
-        // TODO Create a DELETE endpoint that deletes a specific employee
+    public ResponseEntity deleteEmployee(@PathVariable final Long id) {
         return ResponseEntity.accepted().build();
     }
-
-    @PutMapping("{id}")
-    public ResponseEntity updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDto) {
-        //TODO Create a PATCH endpoint that updates an employee with new values
+    @PatchMapping("{id}")
+    public ResponseEntity updateEmployee(@PathVariable final Long id, @RequestBody final EmployeeDto employeeDto) {
         return ResponseEntity.ok().build();
     }
 }
